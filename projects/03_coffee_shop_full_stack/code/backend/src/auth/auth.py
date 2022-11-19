be0124db5266af546819f487f938dcfe672d234c
Mirhire Tarimo <mirhire.t@outlook.com>
2022-11-19 18:15:34 +0000

    updated postman collection, updated auth script / requirements.txt

--- projects/03_coffee_shop_full_stack/code/backend/src/auth/auth.py
+++ projects/03_coffee_shop_full_stack/code/backend/src/auth/auth.py
@@ -151,7 +151,7 @@ def verify_decode_jwt(token):
                 'description': 'Incorrect claims. Please, check the audience and issuer.'
             }, 401)
 
-        except jwt.Exception:
+        except Exception:
             raise AuthError({
                 'code': 'invalid_header',
                 'description': 'Unable to parse authentication token.'
